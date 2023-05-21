5a07f2a18f9dbf30957e4493828a8e3bffa4b5fc
Vasiliy <pilaroiding@gmail.com>
2023-05-21 12:56:58 +0300

    done

--- src/test/java/com/example/calculator/ServiceTest/CalculatorServiceTest.java
+++ src/test/java/com/example/calculator/ServiceTest/CalculatorServiceTest.java
@@ -1,10 +1,12 @@
-package com.coding.hw_spring_firsttime.ServiceTest;
+package com.example.calculator.ServiceTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 import com.coding.hw_spring_firsttime.Service.CalculatorService;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.stream.Stream;
@@ -41,8 +43,71 @@ public class CalculatorServiceTest {
 
 
     }
+    @ParameterizedTest
+    @MethodSource("providedParamsForTestsForAddition")
+    public void ShouldWorkWithStandartNumbersAddition(Integer inputNumber1 , Integer inputNumber2, Integer expected)
+    {
 
+        assertEquals(expected , out.additionCalculator(inputNumber1, inputNumber2));
 
+    }
+    @ParameterizedTest
+    @MethodSource("providedParamsForTestsForSubtraction")
+    public void ShouldWorkWithStandartNumbersSubtraction(Integer inputNumber1 , Integer inputNumber2, Integer expected)
+    {
+
+        assertEquals(expected , out.subtractionCalculator(inputNumber1, inputNumber2));
+
+    }
+    @ParameterizedTest
+    @MethodSource("providedParamsForTestsForMultiplication")
+    public void ShouldWorkWithStandartNumbersMultiplication(Integer inputNumber1 , Integer inputNumber2, Integer expected)
+    {
+
+        assertEquals(expected , out.multiplicationCalculator(inputNumber1, inputNumber2));
+
+    }
+    @ParameterizedTest
+    @MethodSource("providedParamsForTestsForDivision")
+    public void ShouldWorkWithStandartNumbersDivision(Integer inputNumber1 , Integer inputNumber2, Double expected)
+    {
+
+        assertEquals(expected , out.divisionCalculator(inputNumber1, inputNumber2));
+
+    }
+
+    public static Stream<Arguments> providedParamsForTestsForAddition()
+    {
+        return Stream.of(
+                Arguments.of(STANDART_NUMBER_1,STANDART_NUMBER_2,3),
+                Arguments.of(ZERO_NUMBER, STANDART_NUMBER_2, 2)
+        );
+
+    }
+    public static Stream<Arguments> providedParamsForTestsForSubtraction()
+    {
+        return Stream.of(
+                Arguments.of(STANDART_NUMBER_1,STANDART_NUMBER_2,-1),
+                Arguments.of(ZERO_NUMBER, STANDART_NUMBER_2, -2)
+        );
+
+    }
+    public static Stream<Arguments> providedParamsForTestsForDivision()
+    {
+        return Stream.of(
+                Arguments.of(STANDART_NUMBER_1,STANDART_NUMBER_2,0.5D),
+                Arguments.of(ZERO_NUMBER, STANDART_NUMBER_2, 0D)
+        );
+
+    }
+    public static Stream<Arguments> providedParamsForTestsForMultiplication()
+    {
+        return Stream.of(
+                Arguments.of(STANDART_NUMBER_1,STANDART_NUMBER_2,2),
+                Arguments.of(ZERO_NUMBER, STANDART_NUMBER_2, 0)
+        );
+
+    }
 
 
 
